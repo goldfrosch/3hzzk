a2cb8d5e5ca13f555471ef282c007d19ac1f94c4
GoldFrosch <goldfrosch@daum.net>
2024-03-11 00:16:51 +0900

    chore: package.json 검증 및 repo 선택 추가

--- common/cli/src/select.ts
+++ common/cli/src/select.ts
@@ -23,6 +23,28 @@ try {
       return val.toLowerCase();
     },
   });
+
+  const selectDir: string[] = [];
+  // TODO: 추후 root 디렉토리 기반으로 접근하게 끔 수정
+  fs.readdirSync(`../../${packageType}`).forEach((dir) => {
+    const files = fs.readdirSync(`../../${packageType}/${dir}`);
+
+    for (const file of files) {
+      if (file === "package.json") {
+        selectDir.push(dir);
+        break;
+      }
+    }
+  });
+
+  const { theme } = await inquirer.prompt({
+    type: "list",
+    name: "theme",
+    message: "repo를 선택하세요:",
+    // TODO 패키지 타입을 기반으로 디렉토리 탐색 후 packages/[packageType]안의 디렉토리를 탐색 후 노출시켜줌 (바로 안에 package.json이 있을 것)
+    // 가능하면 캐싱 처리해도 좋음 (디렉토리 탐색이 시간이 좀 걸릴 경우에)
+    choices: selectDir,
+  });
 } catch (error) {
   console.error(error);
 }
