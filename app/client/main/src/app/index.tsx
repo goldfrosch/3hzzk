59ce32e6400dea9ea6236a945ec871d553c39d4e
GoldFrosch <goldfrosch@daum.net>
2024-03-11 23:33:13 +0900

    chore: FE 프로젝트 세팅
    1. chore: tanstack-query 세팅
    2. chore: vanilla-extract 세팅

--- app/client/main/src/app/index.tsx
+++ app/client/main/src/app/index.tsx
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "../routeTree.gen";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
+
 // Create a new router instance
 const router = createRouter({ routeTree });
 
@@ -15,6 +20,9 @@ declare module "@tanstack/react-router" {
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
--- app/client/main/vite.config.ts
+++ app/client/main/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite({ routesDirectory: "./src/pages" })],
+  plugins: [
+    react(),
+    vanillaExtractPlugin(),
+    TanStackRouterVite({ routesDirectory: "./src/pages" }),
+  ],
 });
