e247d8526032aee67ed527c400fb0ae1b9b1cb40
GoldFrosch <goldfrosch@daum.net>
2024-03-20 23:57:41 +0900

    refactor: export 처리방식 변경

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,1 +1,4 @@
-export { vars } from "./libs/styles";
+export { default as Button } from "./libs/components/button/button";
+export { default as Card } from "./libs/components/card/card";
+
+export * from "./libs/styles";
